9970f35d252580c9a42e1229e48f0eb3ab270647
Alastaray <rikinikto@gmail.com>
2022-02-01 19:51:21 +0200

    Add class, which has information about users and some files for work with drawing in console.

---
+++ Users.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+
+
+
+
+class User
+{
+public:
+	User(unsigned int id, const char* nickname, const char* password, unsigned int rank);
+	~User();
+	char* GetNickname() { return nickname; }
+	char* GetPassword() { return password; }
+	int GetRank() { return rank; }
+	int GetId() { return id; }
+	void SetNickname(const char* _nickname);
+	void SetPassword(const char* _password);
+	void SetRank(unsigned int _rank);
+protected:
+	char *nickname,
+		 *password;
+	unsigned int id,
+				rank;
+};
